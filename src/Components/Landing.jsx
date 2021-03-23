426e590475b14afe72c7cce37c87da8b85b0b813
KWard <pcward.k@gmail.com>
2021-03-23 15:51:13 -0600

    added router-dom dependencies

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Landing() {
   return (
-    <div>
-      <h1>LANDING PAGE</h1>
+    <div className="landing">
+      <div className="landing-intro">
+        <h1>Keenan Ward</h1>
+        <h2>-</h2>
+        <h1>Full Stack Software Engineer</h1>
+      </div>
     </div>
   );
 }
