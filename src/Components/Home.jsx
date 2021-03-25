8ad9c7def135041b68c5db805533339caeadae87
KWard <pcward.k@gmail.com>
2021-03-25 16:55:57 -0600

    added info to home screen

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,10 +7,19 @@ export default function Home() {
   return (
     <div className="homepage">
       <Header />
-      <h1>
-        Thank you for taking the time to visit my portfolio and to learn about
-        me!
-      </h1>
+      <div className="home-text">
+        <h1>Thank you for taking the time to visit my portfolio!</h1>
+        <h4>
+          This website hosts a breif description of who I am as a person and as
+          a developer. I welcome the opportunity to share some of myself with
+          you! Please contact me if you have any questions, or if I can help
+          with any of your professional needs!
+        </h4>
+        <p id="site-info">
+          This site is programmed in Javascript with the help of my favorite
+          Front-End/UI library; React.js
+        </p>
+      </div>
       <Switch>
         <Route exact path="/about" component={About} />
       </Switch>
