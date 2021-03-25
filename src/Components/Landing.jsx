8803e735ef6db14f1a89b0d6d60ea7afafaafb15
KWard <pcward.k@gmail.com>
2021-03-25 15:14:57 -0600

    added photo to landing, adjusted css

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -4,13 +4,14 @@ import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <div className="landing">
+      <div className="landing-img"></div>
       <div className="landing-intro">
-        <h1>Keenan Ward</h1>
-        <h2>-</h2>
+        <h1 id="name">Keenan Ward</h1>
+        <h2>~</h2>
         <h1>Full Stack Software Engineer</h1>
-      </div>
-      <div className="intro-link">
-        <Link to="/home">Enter Here</Link>
+        <Link to="/home" id="intro-link">
+          Enter Here
+        </Link>
       </div>
     </div>
   );
