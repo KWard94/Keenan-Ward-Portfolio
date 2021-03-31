59a7db6b40b988f27968b6623c37bb610b2b4ead
KWard <pcward.k@gmail.com>
2021-03-31 10:29:26 -0600

    added projects page

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+
+export default function Projects() {
+  return (
+    <div className="projects">
+      <Header />
+      <h1>PROJECTS</h1>
+    </div>
+  );
+}
