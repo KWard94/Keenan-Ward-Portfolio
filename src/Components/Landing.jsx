57048d06113ed76f784a8c2c79a2aaa573ba08a0
KWard <pcward.k@gmail.com>
2021-04-01 14:06:12 -0600

    fully responsive css

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -7,8 +7,8 @@ export default function Landing() {
       <div className="landing-img"></div>
       <div className="landing-intro">
         <h1 id="name">Keenan Ward</h1>
-        <h2>~</h2>
-        <h1>Full Stack Software Engineer</h1>
+        <h2 id="landing-tagline">~</h2>
+        <h1 id="landing-tagline">Full Stack Software Engineer</h1>
         <Link to="/home" id="intro-link">
           Enter
         </Link>
