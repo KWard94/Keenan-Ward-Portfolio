54fd124f6d667f00bfb04005eb02cc75bd4d04d3
KWard <pcward.k@gmail.com>
2021-04-27 09:50:04 -0600

    added pdf functionality to resume

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "./Header";
 import resume from "../images/Resume image.png";
+import { Link } from "react-router-dom";
 
 export default function Resume() {
   return (
     <div className="resume">
       <Header />
       <h1 id="resume-intro">Please Find my Professional Resume Below:</h1>
-      <img src={resume} alt="My Professional Resume" id="resume-img" />
+
+      <Link to="/pdf" target="_blank">
+        <img src={resume} alt="My Professional Resume" id="resume-img" />
+      </Link>
+
+      {/* <iframe src={resumePDF} width="100%" height="700px" title="resume-pdf" /> */}
+
+      {/* <embed src={resumePDF} width="70%" height="900rem" /> */}
     </div>
   );
 }
